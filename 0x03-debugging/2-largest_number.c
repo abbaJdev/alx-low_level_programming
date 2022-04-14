a75057db525b7bd7e3230a05683d76a8c246f6ba
ujawissy4real <ujawissy@gmail.com>
2022-04-14 14:30:45 +0100

    Latest commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -10,24 +10,25 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b)
-	{
-		if (b > c)
-			largest = a;
-		else if (a > c)
-			largest = a;
-		else
-			largest = c;
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
+}
 
-	}
-	else
-	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
-	}
-	return (largest);
+
+return (largest);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,5 +1,1 @@
-# alx-low_level_programming
-# By kentinHogbonouto
-0. Multiple mains
-1. Like, comment, subscribe
-3. Leap year
+**Debuging**
